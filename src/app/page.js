764cbf5871e6aacfd4fb3d4f7817e50fd29c61bd
Poon Boonchoowit <poon15112@gmail.com>
2023-08-28 16:18:06 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,57 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Footer } from "@/components/Footer";
+import {
+  Button,
+  Container,
+  Divider,
+  Group,
+  Pagination,
+  Rating,
+  Text,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
+      <Title order={4} mt="sm">
+        Your rating
+      </Title>
+
+      <Rating size="lg" />
+
+      <Textarea
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        mt="xs"
+        minRows={3}
+      />
+
+      <Button mt="xs" color="orange">
+        Submit Review
+      </Button>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Elon Musk</Title>
+        <Rating size="sm" value={5} readOnly />
+      </Group>
+      <Text c="dimmed" align="center">
+        Best pizza in this world. I give you X score.
+      </Text>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Mark Zuck</Title>
+        <Rating size="sm" value={4} readOnly />
+      </Group>
+      <Text c="dimmed" align="center">
+        My favourite part is pepperoni
+      </Text>
+      <Pagination total={20} color="orange" position="center" mt="md" />
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Poon Boonchoowit 650610786
       </Text>
     </Container>
   );
